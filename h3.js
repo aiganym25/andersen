80be9be100ce0a8ec900ac78ab22b936be4df784
Aiganym <ospan.aiganym@gmail.com>
2023-08-17 17:14:24 +0600

    made few corrections

--- h3.js
+++ h3.js
@@ -1,9 +1,6 @@
 // ex1
 
 Array.prototype.myFilter = function (callBack, obj) {
-  if (typeof callBack !== "function") {
-    throw new Error("Callback must be function");
-  }
   const newArray = [];
   const array = this;
 
@@ -12,14 +9,15 @@ Array.prototype.myFilter = function (callBack, obj) {
       newArray.push(array[i]);
     }
   }
+
   return newArray;
 };
-
 
 // ex2
 
 function createDebounceFunction(callback, delay) {
   let timerId;
+
   return function (...args) {
     clearTimeout(timerId);
     timerId = setTimeout(() => {
