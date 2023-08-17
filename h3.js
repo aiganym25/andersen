f6efdd7ae2fe0c612b187d31b1397f9de5219418
Aiganym <ospan.aiganym@gmail.com>
2023-08-17 15:59:50 +0600

    implemented filter method

--- h3.js
+++ h3.js
@@ -1,2 +1,18 @@
 // ex1
 
+Array.prototype.myFilter = function (callBack, obj) {
+  if (typeof callBack !== "function") {
+    throw new Error("Callback must be function");
+  }
+  const newArray = [];
+  const array = this;
+
+  for (let i = 0; i < array.length; i++) {
+    if (callBack.call(obj, array[i], i, array)) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+
